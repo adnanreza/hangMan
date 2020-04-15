21e6db87590c6a3512007cb2b66f76d0f2dce05f
Adnan Reza <adnan.reza@outlook.com>
2020-04-15 14:49:35 -0700

    Implemented play again button event handler

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ function displayWord() {
   `;
 
   const innerWord = wordEl.innerText.replace(/\n/g, '');
-  console.log(wordEl.innerText, innerWord);
 
   if (innerWord === selectedWord) {
     finalMessage.innerText = 'Congratulations! You won!';
@@ -43,10 +42,27 @@ function displayWord() {
 
 // Update the wrong letters
 function updateWrongLettersEl() {
+  // Display wrong letters on screen as they are typed
   wrongLettersEl.innerHTML = `
     ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
     ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
   `;
+
+  // Display svg parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'You lost :(';
+    popup.style.display = 'flex';
+  }
 }
 
 // Show notificaiton
@@ -78,6 +94,18 @@ window.addEventListener('keydown', (e) => {
       }
     }
   }
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+  displayWord();
+  updateWrongLettersEl();
+  popup.style.display = 'none';
 });
 
 displayWord();
